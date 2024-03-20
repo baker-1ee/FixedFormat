27b6e11a4f6d916b9aa2f2a0c3bc9864bb85d8d0
baker.lee <ljw722007@gmail.com>
2024-03-20 22:37:32 +0900

    test code

--- src/test/java/com/example/fixedformat/converter/FixedFormatConverterTest.java
+++ src/test/java/com/example/fixedformat/converter/FixedFormatConverterTest.java
@@ -1,5 +1,30 @@
 package com.example.fixedformat.converter;
 
+import com.example.fixedformat.component.FixedFormatFactory;
+import com.example.fixedformat.converter.mock.FixedFormatHomeTaxFormatter;
+import com.example.fixedformat.converter.mock.fixedformat.MockDoubleNestedFixedFormat;
+import com.example.fixedformat.converter.mock.fixedformat.MockNestedFixedFormat;
+import com.example.fixedformat.converter.mock.fixedformat.MockVatFixedFormat;
+import com.example.fixedformat.converter.mock.parser.MockFixedFormatReadParser;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest(classes = FixedFormatFactory.class)
 class FixedFormatConverterTest {
+
+    @Autowired
+    FixedFormatFactory fixedFormatFactory;
+
+    FixedFormatConverter<MockVatFixedFormat> fixedFormatConverter;
+    FixedFormatConverter<MockNestedFixedFormat> nestedFixedFormatConverter;
+    FixedFormatConverter<MockDoubleNestedFixedFormat> doubleNestedFixedFormatConverter;
+
+    @PostConstruct
+    void init() {
+        fixedFormatConverter = fixedFormatFactory.getConverter(new FixedFormatHomeTaxFormatter(), new MockFixedFormatReadParser());
+    }
 
 }
---
+++ src/test/java/com/example/fixedformat/converter/mock/FixedFormatHomeTaxFormatter.java
@@ -0,0 +1,6 @@
+package com.example.fixedformat.converter.mock;
+
+import com.example.fixedformat.formatter.FixedFormatDefaultFormatter;
+
+public class FixedFormatHomeTaxFormatter extends FixedFormatDefaultFormatter {
+}
